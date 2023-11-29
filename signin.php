8496b6a3f56801c85450ed4dfec1b17ffe74ae3b
Kingestif <kingestiff@gmail.com>
2023-11-29 05:39:09 +0300

    made sign in form

---
+++ signin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signin</title>
+    <link rel=stylesheet href=signin.css>
+</head>
+<body>
+    <form>
+        <p>
+            <label for=uname>Username:</label>
+            <input type=text placeholder=username id=uname required>
+        </p>
+        <p>
+            <label for=email>email:</label>
+            <input type=email placeholder=email id=email required>
+        </p>
+        <button>Sign in</button>
+
+    </form>
+    
+</body>
+</html>
