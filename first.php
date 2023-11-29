bf526e32fb8583854096486bdd7c158321117bd1
Kingestif <kingestiff@gmail.com>
2023-11-29 05:35:59 +0300

    created Json file

--- first.php
+++ first.php
@@ -4,8 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="first.css">
 </head>
 <body>
+    <h1>Welcome!</h1>
+
+    <h2><a href=signin.php>Sign in </a>| <a href=register.php>Register </a></h2>
+
     <?php
         echo("hello world");
     ?>
